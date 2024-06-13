04fc206f0ee7733835843147b05dbc91c13c4c02
opeariyo <opemiariyo@gmail.com>
2024-06-13 12:24:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// var school = "Shekinah School"
+// var name = "Feyi Love"
+
+// console.log(school)
+// console.log(name)
+
+//  let course = "data analysis"
+//  let age = 70
+
+//  console.log(course)
+//  console.log(age)
+
+// const year = "2030"
+// const group = "QA MAY COHORT"
+
+// console.log(year)
+// console.log(group)
+
+// alert('JESUS IS LORD! JESUS LOVES YOU!')
+
+// alert('From SHEKINAH COVENANT CHURCH WORLDWIDE')
+
+// alert('we say remember JESUS IS COMING BACK AGAIN! ...STAY RAPTURABLE')
+
+
+const dayOftheweek = "Saturday"
+const numberOfday = "6th day of the week"
+const sentence = 'Today is ' + dayOftheweek + ', ' + 'which is the ' + numberOfday + '.'
+
+
+console.log(dayOftheweek)
+console.log(numberOfday)
+console.log(sentence)
